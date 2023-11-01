7df7565d3d2263d1e0f6b8be951b5140741e83ed
Gowtham11234 <118426147+Gowtham11234@users.noreply.github.com>
2023-11-01 17:18:03 +0530

    code updated

--- src/main/java/com/fssa/turbotrip/dao/BookingDAO.java
+++ src/main/java/com/fssa/turbotrip/dao/BookingDAO.java
@@ -1,15 +1,14 @@
 package com.fssa.turbotrip.dao;
 
-import java.sql.Connection;
+import java.sql.Connection; 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
+
 import java.util.List;
-import java.util.Map;
+
 
 import com.fssa.turbotrip.dao.exception.DAOException;
 import com.fssa.turbotrip.model.Booking;
--- src/main/java/com/fssa/turbotrip/service/UserService.java
+++ src/main/java/com/fssa/turbotrip/service/UserService.java
@@ -114,5 +114,6 @@ public class UserService {
 			throw new ServiceException(e.getMessage());
 		}
 	}
+	
 
 }
