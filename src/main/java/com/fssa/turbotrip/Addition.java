7d8552fd444f9627b952716eb24ebb9285a08f38
Gowtham11234 <118426147+Gowtham11234@users.noreply.github.com>
2023-11-01 16:21:50 +0530

    code updated

---
+++ src/main/java/com/fssa/turbotrip/Addition.java
@@ -0,0 +1,7 @@
+package com.fssa.turbotrip;
+
+public class Addition {
+	public static int add(int a, int b) {
+		return a+b;
+	}
+}
---
+++ src/main/java/com/fssa/turbotrip/Demo.java
@@ -0,0 +1,11 @@
+package com.fssa.turbotrip;
+
+public class Demo {
+	
+public static void main(String [] args) {
+	int a = 10;
+	int b = 20;
+	int c=Addition.add(a,b);
+	System.out.println(c);
+}
+}
